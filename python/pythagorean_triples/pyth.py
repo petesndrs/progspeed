1b31636d29d667c4f46fb578a7e9bc441118f0c1
petesndrs <petesndrs@users.noreply.github.com>
2018-12-06 22:59:36 +0000

    Use python class rather than dictionary

--- python/pythagorean_triples/pyth.py
+++ python/pythagorean_triples/pyth.py
@@ -1,4 +1,12 @@
 
+class Triple:
+
+    def __init__(self):
+        self.a = None
+        self.b = None
+        self.c = None
+        self.valid = True
+
 def search(a, b, start, end, inc):
 
     for c in range(start, end, inc):
@@ -11,14 +19,14 @@ def search(a, b, start, end, inc):
 def remove_multiples(found, num):
 
     for i in range(0, num):
-        if found[i]['valid']:
+        if found[i].valid:
             k = 2
             for j in range(i+1, num):
-                if found[j]['valid']:
-                    if ( found[i]['a'] * k == found[j]['a'] and
-                         found[i]['b'] * k == found[j]['b'] and
-                         found[i]['c'] * k == found[j]['c'] ):
-                      found[j]['valid'] = False
+                if found[j].valid:
+                    if ( found[i].a * k == found[j].a and
+                         found[i].b * k == found[j].b and
+                         found[i].c * k == found[j].c ):
+                      found[j].valid = False
                       k += 1
 
 def main():
@@ -26,7 +34,7 @@ def main():
     MIN = 1
     MAX = 2000
     MAX_TRIPLES = 4000
-    found = [{} for i in range(MAX_TRIPLES)]
+    found = [Triple() for i in range(MAX_TRIPLES)]
 
     """ c**2 = a**2 + b**2 """
 
@@ -36,17 +44,16 @@ def main():
             c = search (a, b, b+1, MAX, 1)
             if (c):
                 print('{}, {}, {}, {}'.format(found_triples, a, b, c))
-                found[found_triples]['a'] = a
-                found[found_triples]['b'] = b
-                found[found_triples]['c'] = c
-                found[found_triples]['valid'] = True
+                found[found_triples].a = a
+                found[found_triples].b = b
+                found[found_triples].c = c
                 found_triples += 1
 
     remove_multiples(found, found_triples)
 
     for i in range(0, found_triples):
-        if found[i]['valid']:
-            print('{}, {}, {}, {}'.format(i, found[i]['a'], found[i]['b'], found[i]['c']))
+        if found[i].valid:
+            print('{}, {}, {}, {}'.format(i, found[i].a, found[i].b, found[i].c))
 
 
 if __name__ == "__main__":
