26bf012b79142bd7f0a8fa384271be49e909887a
petesndrs <petesndrs@users.noreply.github.com>
2018-12-06 22:00:02 +0000

    Add go compilation and run to run.ps1

--- run.ps1
+++ run.ps1
@@ -2,15 +2,27 @@
 
 $Env:path = $env:path + ";c:\MinGW\bin"
 $Env:path = $env:path + ";c:\Python34\"
+$Env:path = $env:path + ";c:\Go\bin\"
 
 cc --version
 python --version
+go version
 
 date
 
 pushd c\pythagorean_triples
 
 cc -std=c99 -o pyth.exe pyth.c
+
+Measure-Command {.\pyth.exe > pyth.out}
+
+popd
+
+date
+
+pushd go\pythagorean_triples
+
+go build -o pyth.exe pyth.go
 
 Measure-Command {.\pyth.exe > pyth.out}
 
