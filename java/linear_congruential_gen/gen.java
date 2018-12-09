174fb11683cdce2a2fa2ec5ee7fe4d6e879a1003
petesndrs <petesndrs@users.noreply.github.com>
2018-12-09 18:59:20 +0000

    Add java linear congruential generator

---
+++ java/linear_congruential_gen/gen.java
@@ -0,0 +1,21 @@
+class gen 
+{
+    public static void main(String args[])
+    {
+        final int SEED = 0;
+        int state = SEED;
+        int val = 1;
+        long period = 0;
+
+        while (val != SEED){
+            val = ((state * 1103515245) + 12345) & 0x7fffffff;
+            if (val < 0x100){
+                System.out.printf("period %d state %x, val %x\n", period, state, val);
+            }
+            period++;
+            state = val;
+        }
+
+        System.out.printf("%d\n", period);
+    }
+}
--- run.ps1
+++ run.ps1
@@ -3,10 +3,13 @@
 $Env:path = $env:path + ";c:\MinGW\bin"
 $Env:path = $env:path + ";c:\Python34\"
 $Env:path = $env:path + ";c:\Go\bin\"
+$Env:path = $env:path + ";c:\Program Files\Java\jre1.8.0_161\bin;c:\Program Files\Java\jdk1.7.0_60"
 
 cc --version
 python --version
 go version
+javac -version
+java -version
 
 date
 
@@ -41,6 +44,16 @@ pushd go\linear_congruential_gen
 go build -o gen.exe gen.go
 
 Measure-Command {.\gen.exe > gen.out}
+
+popd
+
+date
+
+pushd java\linear_congruential_gen
+
+javac gen.java
+
+Measure-Command {java gen > gen.out}
 
 popd
 
