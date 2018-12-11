27a8e185f12e41ca98c884ddaa1edeb4bf7e46fb
petesndrs <petesndrs@users.noreply.github.com>
2018-12-11 22:50:27 +0000

    remove semi-colons from go source

--- go/linear_congruential_gen/gen.go
+++ go/linear_congruential_gen/gen.go
@@ -4,20 +4,20 @@ import "fmt"
 
 func main() {
     
-    const SEED int = 0;
-    var state int = SEED;
-    var val int = 1;
-    var period int = 0;
+    const SEED int = 0
+    var state int = SEED
+    var val int = 1
+    var period int = 0
 
     for val != SEED {
-        val = ((state * 1103515245) + 12345) & 0x7fffffff;
+        val = ((state * 1103515245) + 12345) & 0x7fffffff
         if val < 0x100 {
-            fmt.Printf("period %d state %x, val %x\n", period, state, val);
+            fmt.Printf("period %d state %x, val %x\n", period, state, val)
         }
-        period++;
-        state = val;
+        period++
+        state = val
     }
 
-    fmt.Printf("%d\n", period);
+    fmt.Printf("%d\n", period)
 
 }
