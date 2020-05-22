1b7d1122c42e055cffb4762b8c8468e0738371b9
Peter Sanders <peter.sanders@youview.com>
2020-05-22 18:47:13 +0100

    run.sh diff results and make output ascii versions

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 gcc --version
-python3 --version
+pypy3 --version
 go version
 javac -version
 java -version
 
-DIR=("pythagorean_triples" "linear_congruential_gen" "xor_shift_gen" "fibonacci_twoways" "kaprekars_process")
-BIN=("pyth" "gen" "xor" "fib" "kap")
+DIR=("pythagorean_triples" "linear_congruential_gen" "xor_shift_gen" "fibonacci_twoways" "kaprekars_process" "naughts_crosses")
+BIN=("pyth" "gen" "xor" "fib" "kap" "nac")
 
 date
 
@@ -20,7 +20,8 @@ do
 
   gcc -std=c99 -o ${BIN[$i]}.exe ${BIN[$i]}.c
 
-  time -p ./${BIN[$i]}.exe > ${BIN[$i]}.out
+  time ./${BIN[$i]}.exe > ${BIN[$i]}.out
+  diff ${BIN[$i]}.out ../../samples/${DIR[$i]}/${BIN[$i]}.out
 
   popd >> /dev/null
 
@@ -28,7 +29,8 @@ do
 
   go build -o ${BIN[$i]}.exe ${BIN[$i]}.go
 
-  time -p ./${BIN[$i]}.exe > ${BIN[$i]}.out
+  time ./${BIN[$i]}.exe > ${BIN[$i]}.out
+  diff ${BIN[$i]}.out ../../samples/${DIR[$i]}/${BIN[$i]}.out
 
   popd >> /dev/null
 
@@ -36,13 +38,15 @@ do
 
   javac ${BIN[$i]}.java
 
-  time -p java ${BIN[$i]} > ${BIN[$i]}.out
+  time java ${BIN[$i]} > ${BIN[$i]}.out
+  diff ${BIN[$i]}.out ../../samples/${DIR[$i]}/${BIN[$i]}.out
 
   popd >> /dev/null
 
   pushd python/${DIR[$i]} >> /dev/null
 
-  time -p python ${BIN[$i]}.py > ${BIN[$i]}.out
+  time pypy3 ${BIN[$i]}.py > ${BIN[$i]}.out
+  diff ${BIN[$i]}.out ../../samples/${DIR[$i]}/${BIN[$i]}.out
 
   popd >> /dev/null
 
