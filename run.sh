c2ec775c1f960eae80d8cb994816e5319e4f6298
Peter Sanders <peter.sanders@youview.com>
2020-06-19 10:03:56 +0100

    Update run.sh shell script

--- run.sh
+++ run.sh
@@ -6,8 +6,12 @@ go version
 javac -version
 java -version
 
-DIR=("pythagorean_triples" "linear_congruential_gen" "xor_shift_gen" "fibonacci_twoways" "kaprekars_process" "naughts_crosses")
-BIN=("pyth" "gen" "xor" "fib" "kap" "nac")
+DIR=("pythagorean_triples" "linear_congruential_gen" "xor_shift_gen" "fibonacci_twoways"
+     "kaprekars_process" "naughts_crosses" "triangle_enclosure" "unique_anagram"
+     "goldbach_conjecture" "sieve_eratosthenes")
+
+BIN=("pyth" "gen" "xor" "fib" "kap" "nac" "tri" "ana"
+     "gold" "erat")
 
 date
 
