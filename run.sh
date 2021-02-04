d2daaa836c8b2bf3e9830d2e7c38300045d9a488
Peter Sanders <peter.sanders@youview.com>
2021-02-04 20:17:57 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,10 +8,10 @@ java -version
 
 DIR=("pythagorean_triples" "linear_congruential_gen" "xor_shift_gen" "fibonacci_twoways"
      "kaprekars_process" "naughts_crosses" "triangle_enclosure" "unique_anagram"
-     "goldbach_conjecture" "sieve_eratosthenes" "pi_from_random")
+     "goldbach_conjecture" "sieve_eratosthenes" "pi_from_random" "min_spanning_tree")
 
 BIN=("pyth" "gen" "xor" "fib" "kap" "nac" "tri" "ana"
-     "gold" "erat" "pirn")
+     "gold" "erat" "pirn" "mst")
 
 date
 
